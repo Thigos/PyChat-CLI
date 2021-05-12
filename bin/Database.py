892849f29eb01e10e34ca3e22df257879c5708d5
Thiago M <67590378+Thigos@users.noreply.github.com>
2021-05-12 10:53:48 -0300

    v0.1-12/05/2021

--- bin/Database.py
+++ bin/Database.py
@@ -2,14 +2,13 @@ import pyrebase
 
 #Configuração do Firebase
 firebaseConfig = {
-    "apiKey": "AIzaSyDOlZ0ReztZB6e1iU8RvaWJXZcsuIW8now",
-    "authDomain": "avaliacao-eb2ec.firebaseapp.com",
-    "databaseURL": "https://avaliacao-eb2ec.firebaseio.com",
-    "projectId": "avaliacao-eb2ec",
-    "storageBucket": "avaliacao-eb2ec.appspot.com",
-    "messagingSenderId": "42796567000",
-    "appId": "1:42796567000:web:12eb72d28642eed3098aad",
-    "measurementId": "G-W4X1VE8JXK"
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appID: "app-id",   
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
